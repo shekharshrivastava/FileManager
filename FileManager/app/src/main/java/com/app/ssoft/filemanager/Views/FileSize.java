aef718fc92a74da75d46d7a237a81bbf11b27905
shekharshrivastava <ershrivastavashekhar@gmail.com>
2018-02-21 18:29:03 +0530

    added pojo class for fileSize

---
+++ FileManager/app/src/main/java/com/app/ssoft/filemanager/Views/FileSize.java
@@ -0,0 +1,44 @@
+package com.app.ssoft.filemanager.Views;
+
+/**
+ * Created by Shekahar.Shrivastava on 21-Feb-18.
+ */
+
+public class FileSize {
+    float docFIleSize;
+    float imageFilesSize;
+    float videoFilesSize;
+    float audioFileSize;
+
+    public float getDocFIleSize() {
+        return docFIleSize;
+    }
+
+    public void setDocFIleSize(float docFIleSize) {
+        this.docFIleSize = docFIleSize;
+    }
+
+    public float getImageFilesSize() {
+        return imageFilesSize;
+    }
+
+    public void setImageFilesSize(float imageFilesSize) {
+        this.imageFilesSize = imageFilesSize;
+    }
+
+    public float getVideoFilesSize() {
+        return videoFilesSize;
+    }
+
+    public void setVideoFilesSize(float videoFilesSize) {
+        this.videoFilesSize = videoFilesSize;
+    }
+
+    public float getAudioFileSize() {
+        return audioFileSize;
+    }
+
+    public void setAudioFileSize(float audioFileSize) {
+        this.audioFileSize = audioFileSize;
+    }
+}
